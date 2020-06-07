d818685ace007b0f80646db99cf876407855e7f9
Pace Willisson <pace@alum.mit.edu>
2020-06-07 14:53:07 -0400

    mod

--- mod_pace.lua
+++ mod_pace.lua
@@ -2,6 +2,11 @@ local st = require "util.stanza";
 local jid = require "util.jid";
 local nodeprep = require "util.encodings".stringprep.nodeprep;
 
+-- local get_room_from_jid = module:require "util".get_room_from_jid;
+
+local all_rooms = module:shared "muc/all-rooms";
+local live_rooms = module:shared "muc/live_rooms";
+
 local rooms = module:shared "muc/rooms";
 if not rooms then
         module:log("error", "This module only works on MUC components!");
@@ -9,9 +14,44 @@ if not rooms then
 end
 
 local function pace_block(room)
-   return nil
+   return nil;
 end
 
+
+function indent(level)
+   local s = "";
+   local i;
+   for i = 1,level do
+      s = s .. '  ';
+   end
+   return s;
+end
+
+tbl_seen = {}
+
+function dump(o, level)
+   if level > 3 then
+      return indent(level) .. "...\n"
+   end
+
+   if type(o) == 'table' then
+      if tbl_seen[o] then
+	 return indent(level) .. "LOOP\n"
+      end
+      tbl_seen[o] = true
+      s = ""
+      for k,v in pairs(o) do
+	 idx = k
+         if type(k) ~= 'number' then idx = '"'..k..'"' end
+	 s = s .. indent(level) .. '[' .. idx .. ']\n'
+	 s = s .. dump(v, level+1)
+      end
+      return s
+   else
+      s = indent(level) .. tostring(o) .. "\n"
+      return s
+   end
+end
 
 module:hook("presence/full", function(event)
         local stanza = event.stanza;
@@ -24,8 +64,8 @@ module:hook("presence/full", function(event)
 	local room = jid.split(stanza.attr.to);
         if not room then return; end
 
-        module:log("error", "room %s", room);
-	
+        module:log("error", "** stanza.attr.from %s", stanza.attr.from);
+        module:log("error", "** stanza.attr.to %s", stanza.attr.to);
 
 	if pace_block(room) then
                 event.allowed = false;
@@ -37,3 +77,15 @@ module:hook("presence/full", function(event)
 				  "invalid room name"));
         end
 end, 10);
+
+module:log("error", "** hello");
+local hashes = require "util.hashes";
+local base64_encode = require "util.encodings".base64.encode;
+local base64_decode = require "util.encodings".base64.decode;
+
+key = "xyzzy";
+msg = "hello"
+
+h = base64_encode (hashes.hmac_sha256(key, msg));
+
+module:log("error", "** hash %s", h);
